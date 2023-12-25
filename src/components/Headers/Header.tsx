bd0c397238a3c7c0a47279ed68999caf3b34d004
Pr1yansu <priyansuchowdhury57@gmail.com>
2023-12-25 12:15:55 +0530

    changes

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -1,46 +1,58 @@
 import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [token, setToken] = useState<string | null>(null);
+
+  const Links = [
+    {
+      to: "/",
+      name: "Home",
+    },
+    {
+      to: token ? "/logout" : "/login",
+      name: token ? "Logout" : "Login",
+      onClick: () => {
+        if (!token) return;
+        localStorage.removeItem("auth-token");
+        setToken(null);
+        window.location.href = "/";
+      },
+    },
+    {
+      to: "/dashboard",
+      name: "Dashboard",
+    },
+    {
+      to: "/cart",
+      name: "Cart",
+    },
+  ];
+  useEffect(() => {
+    const authToken = localStorage.getItem("auth-token");
+    setToken(authToken);
+  }, []);
   return (
     <div className="header">
       <Link to="/" className="logo">
         LOGO
       </Link>
-      <div className="nav-links">
-        <NavLink
-          to="/"
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "pending" : ""
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/login"
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "pending" : ""
-          }
-        >
-          Login
-        </NavLink>
-        <NavLink
-          to="/dashboard"
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "pending" : ""
-          }
-        >
-          Dashboard
-        </NavLink>
-        <NavLink
-          to="/cart"
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "pending" : ""
-          }
-        >
-          Cart
-        </NavLink>
-      </div>
+      <ul className="nav-links">
+        {Links.map((link, index) => (
+          <li key={index}>
+            <NavLink
+              to={link.to}
+              className={({ isActive, isPending }) =>
+                isActive ? "active" : isPending ? "pending" : ""
+              }
+              onClick={link.onClick}
+            >
+              {link.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
